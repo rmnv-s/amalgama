35c39504c1777986ac73aad6ea641d18b07b62d3
Stas Remnev <remnev.dev@gmail.com>
2024-05-18 14:07:52 +0300

    fix: main-screen

--- src/js/script.js
+++ src/js/script.js
@@ -53,70 +53,6 @@ menuLinks.forEach((link) => {
   });
 });
 
-// Анимация картинки на главном экране
-const app = new PIXI.Application({
-  width: window.innerWidth,
-  height: window.innerHeight,
-  backgroundColor: 0xffffff,
-  // resizeTo: window,
-});
-
-document.querySelector(".hero__pixi-container").appendChild(app.view);
-
-// Загрузка текстур
-const imgTexture = PIXI.Texture.from("/img/A-main.png");
-const depthTexture = PIXI.Texture.from("/img/A-main-depth.png");
-
-imgTexture.baseTexture.on("loaded", () => {
-  depthTexture.baseTexture.on("loaded", () => {
-    // Создание спрайта из основного изображения
-    const img = new PIXI.Sprite(imgTexture);
-    img.anchor.set(0.5);
-    img.x = app.screen.width / 2;
-    img.y = app.screen.height / 2;
-    app.stage.addChild(img);
-
-    // Создание спрайта для карты глубины
-    const depthMap = new PIXI.Sprite(depthTexture);
-    depthMap.anchor.set(0.05);
-    // depthMap.x = app.screen.width / 2;
-    // depthMap.y = app.screen.height / 2;
-    app.stage.addChild(depthMap);
-
-    // Создание фильтра смещения
-    const displacementFilter = new PIXI.filters.DisplacementFilter(depthMap);
-    // displacementFilter.autoFit = true;
-    // displacementFilter.scale.set(50, 50); // начальная настройка масштаба
-    img.filters = [displacementFilter];
-
-    // Скрыть карту глубины
-    // depthMap.visible = false;
-
-    // Обработчик движения мыши
-    window.onmousemove = function (e) {
-      const mouseX = e.clientX;
-      const mouseY = e.clientY;
-
-      const centerX = app.screen.width / 2;
-      const centerY = app.screen.height / 2;
-
-      const deltaX = centerX - mouseX;
-      const deltaY = centerY - mouseY;
-
-      displacementFilter.scale.x = deltaX / 20;
-      displacementFilter.scale.y = deltaY / 20;
-    };
-
-    // Обработчик изменения размера окна
-    window.addEventListener("resize", () => {
-      // img.x = app.screen.width / 2;
-      // img.y = app.screen.height / 2;
-      // depthMap.x = app.screen.width / 2;
-      // depthMap.y = app.screen.height / 2;
-    });
-  });
-});
-
 // document.addEventListener("DOMContentLoaded", function () {
 const listItems = document.querySelectorAll(".card__list-item");
 
@@ -149,3 +85,79 @@ listItems.forEach(function (item) {
   });
 });
 // });
+
+// // import * as PIXI from "./pixi.js"; // Анимация картинки на главном экране
+//
+// console.log(PIXI);
+// const app = new PIXI.Application({
+//   width: window.innerWidth,
+//   height: window.innerHeight,
+//   backgroundColor: 0xffffff,
+//   // resizeTo: window,
+// });
+//
+// document.querySelector(".hero__pixi-container").appendChild(app.view);
+//
+// // Загрузка текстур
+// const imgTexture = PIXI.Texture.from("/img/A-main.png");
+// const depthTexture = PIXI.Texture.from("/img/A-main-depth.png");
+//
+// imgTexture.baseTexture.on("loaded", () => {
+//   depthTexture.baseTexture.on("loaded", () => {
+//     const img = new PIXI.Sprite(imgTexture);
+//     img.anchor.set(0.5);
+//     img.x = app.screen.width / 2;
+//     img.y = app.screen.height / 2;
+//     app.stage.addChild(img);
+//
+//     // Создание спрайта для карты глубины
+//     const depthMap = new PIXI.Sprite(depthTexture);
+//     depthMap.anchor.set(0.05);
+//     depthMap.x = app.screen.width / 2;
+//     depthMap.y = app.screen.height / 2;
+//     app.stage.addChild(depthMap);
+//
+//     // Создание фильтра смещения
+//     const displacementFilter = new PIXI.filters.DisplacementFilter(depthMap);
+//     // displacementFilter.autoFit = true;
+//     displacementFilter.scale.set(50, 50); // начальная настройка масштаба
+//     img.filters = [displacementFilter];
+//
+//     // Скрыть карту глубины
+//     depthMap.visible = false;
+//
+//     // Обработчик движения мыши
+//     window.onmousemove = function (e) {
+//       const mouseX = e.clientX;
+//       const mouseY = e.clientY;
+//
+//       const centerX = app.screen.width / 2;
+//       const centerY = app.screen.height / 2;
+//
+//       const deltaX = centerX - mouseX;
+//       const deltaY = centerY - mouseY;
+//
+//       displacementFilter.scale.x = deltaX / 20;
+//       displacementFilter.scale.y = deltaY / 20;
+//     };
+//   });
+// });
+// // Создание приложения PixiJS
+//
+// // Загрузка текстуры
+// // const img = new PIXI.Sprite.from("/img/A-main.png");
+// // img.width = window.innerWidth;
+// // img.height = window.innerHeight;
+// // app.stage.addChild(img);
+// //
+// // depthMap = new PIXI.Sprite.from("/img/A-main-depth.png");
+// // app.stage.addChild(depthMap);
+// //
+// // displacementFilter = new PIXI.filters.DisplacementFilter(depthMap);
+// // app.stage.filters = [displacementFilter];
+// //
+// // //
+// // window.onmousemove = function (e) {
+// //   displacementFilter.scale.x = (window.innerWidth / 2 - e.clientX) / 20;
+// //   displacementFilter.scale.y = (window.innerHeight / 2 - e.clientX) / 20;
+// // };
